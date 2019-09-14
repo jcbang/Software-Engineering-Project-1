1cc5bb2b0c6cb6392387a8f9ce79b0f8a112d8a7
Jacob <jpeterson976@gmail.com>
2019-09-13 22:34:51 -0400

    Abstracted away database info

--- server.js
+++ server.js
@@ -7,17 +7,18 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const db = require('./config/keys').mongoURI;
+
 // Connect to Mongo (this is promise based)
 mongoose
-    // .connect("mongodb+srv://JCBang:fgT!aETWybGke5H@ucf-spc-poosw-project1-exub3.azure.mongodb.net/test?retryWrites=true&w=majority\n")
-    .connect("mongodb+srv://JCBang:G73XgEPumDZEMGJm@ucf-spc-poosw-project1-exub3.azure.mongodb.net/contact_manager?retryWrites=true&w=majority")
-    .then(() => console.log('MongoDB Connected!'))
-    .catch(err => console.log(err))
+  .connect(db)
+  .then(() => console.log('MongoDB Connected!'))
+  .catch(err => console.log(err));
 
 // Use routes
-app.use('/contacts', require('./routes/api/contacts'))
+app.use('/contacts', require('./routes/api/contacts'));
 app.use('/user', require('./routes/api/user'));
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started up on ${port}`))
+app.listen(port, () => console.log(`Server started up on ${port}`));
