83573e44523a590893afe98ab031f873b043e182
Justin <justin.c.bang@gmail.com>
2019-09-18 00:32:05 -0400

    Ports Update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const db = require('./config/keys').mongoURI; // our access key for the database
+const db = require('./config/keys').mongoURI; // our access key for the database
 
 // Attempt to pull from Heroku
 const aws = require('aws-sdk');
@@ -18,8 +18,8 @@ app.use(bodyParser.json());
 
 // Connect to Mongo (this is promise based)
 mongoose
-	// .connect(db)
-	.connect(String(s3.db), { useNewUrlParser: true })
+	.connect(db)
+	//.connect(String(s3.db), { useNewUrlParser: true })
 	.then(() => console.log('MongoDB Connected!'))
 	.catch(err => console.log(err));
 
@@ -31,6 +31,6 @@ app.use('/contacts', contacts);
 // app.listen(port, () => console.log(`Server started up on ${port}`));
 
 // Dynamic Heroku Port replaces the above two lines of code
-app.listen(process.env.PORT || 5000, function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
