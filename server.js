d7264025bf0f8f6e6823e6d2cebc92c65621c7a3
Jacob Peterson <jpeterson976@gmail.com>
2019-09-18 22:59:49 -0400

    Switched to localhost connection for testing

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const db = require('./config/keys').mongoURI; // our access key for the database
+const db = require('./config/keys').mongoURI; // our access key for the database
 
 // Attempt to pull from Heroku
-const aws = require('aws-sdk');
-let s3 = new aws.S3({
-  db: process.env.mongoURI
-});
+// const aws = require('aws-sdk');
+// let s3 = new aws.S3({
+//   db: process.env.mongoURI
+// });
 
 const user = require('./routes/api/user');
 const contacts = require('./routes/api/contacts');
@@ -18,8 +18,8 @@ app.use(bodyParser.json());
 
 // Connect to Mongo (this is promise based)
 mongoose
-	//.connect(db)
-	.connect(String(s3.db), { useNewUrlParser: true })
+	.connect(db)
+	// .connect(String(s3.db), { useNewUrlParser: true })
 	.then(() => console.log('MongoDB Connected!'))
 	.catch(err => console.log(err));
 
@@ -31,6 +31,6 @@ app.use('/contacts', contacts);
 // app.listen(port, () => console.log(`Server started up on ${port}`));
 
 // Dynamic Heroku Port replaces the above two lines of code
-app.listen(process.env.PORT || 5000, function(){
+app.listen(process.env.PORT || 5000, function () {
 	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+});
