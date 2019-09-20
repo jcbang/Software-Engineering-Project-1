08e881b6cdef3433323ccbdaefa2a64c49f3d6f3
Justin <justin.c.bang@gmail.com>
2019-09-20 11:53:37 -0400

    Update API

--- server.js
+++ server.js
@@ -30,25 +30,20 @@ app.use('/api/user', user);
 app.use('/api/contacts', contacts);
 
 app.get('/api/hello/', (req, res) => {
-	res.send({ express: 'Hello From Express' });
+	res.json({ express: 'Hello From Express' });
 });
   
 app.post('/api/world/', (req, res) => {
 	console.log(req.body);
-	res.send(
+	res.json(
 	  `I received your POST request. This is what you sent me: ${req.body.post}`,
 	);
 });
 
-if (process.env.NODE_ENV === 'production') 
-{
-	// Serve any static files
-	app.use(express.static(path.join(__dirname, 'frontend/build')));
-  // Handle React routing, return all requests to React app
-	app.get('/', function(req, res) {
-	  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-	});
-}
+// Anything that doesn't match the above, send back the index.html file
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
 // Dynamic Heroku Port replaces the above two lines of code
 app.listen(process.env.PORT || 5000, function(){
