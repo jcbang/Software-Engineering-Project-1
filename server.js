3779bfaf10467ee94370f2350c48aa1b389b270e
Justin <justin.c.bang@gmail.com>
2019-09-20 11:45:59 -0400

    GET request update

--- server.js
+++ server.js
@@ -29,11 +29,11 @@ mongoose
 app.use('/api/user', user);
 app.use('/api/contacts', contacts);
 
-app.get('/api/hello', (req, res) => {
+app.get('/api/hello/', (req, res) => {
 	res.send({ express: 'Hello From Express' });
 });
   
-app.post('/api/world', (req, res) => {
+app.post('/api/world/', (req, res) => {
 	console.log(req.body);
 	res.send(
 	  `I received your POST request. This is what you sent me: ${req.body.post}`,
